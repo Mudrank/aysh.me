8a73de202fecfd82c2e086c09a3120bfe97d79ed
Ayush Pathak <62694274+ayshptk@users.noreply.github.com>
2021-05-07 23:54:05 +0530

    Glitch beautify + edits + blog init

--- p/server.js
+++ p/server.js
@@ -1,21 +1,33 @@
-lol = Math.floor(Math.random() * 7);
-cursor = ["⚡", "✨", "🦄", "🌈", "🍄", "🥐", "🛴"]
-yo = cursor[lol]
-console.log(yo)
-document.getElementsByTagName('body')[0].style.cursor =
-  `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'  width='50' height='60' viewport='0 0 100 100' style='fill:black;font-size:30px;'><text y='50%'>${yo}</text></svg>") 16 0, default`
-console.log(document.body.style)
+var lol = Math.floor(Math.random() * 7);
+var cursor = ["⚡", "✨", "🦄", "🌈", "🍄", "🥐", "🛴"];
+var yo = cursor[lol];
+console.log(yo);
+document.getElementsByTagName(
+  "body"
+)[0].style.cursor = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'  width='50' height='60' viewport='0 0 100 100' style='fill:black;font-size:30px;'><text y='50%'>${yo}</text></svg>") 16 0, default`;
+console.log(document.body.style);
 var keyc = {
-  37: 'left',
-  38: 'up',
-  39: 'right',
-  40: 'down',
-  65: 'a',
-  66: 'b'
+  37: "left",
+  38: "up",
+  39: "right",
+  40: "down",
+  65: "a",
+  66: "b"
 };
-var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+var konamiCode = [
+  "up",
+  "up",
+  "down",
+  "down",
+  "left",
+  "right",
+  "left",
+  "right",
+  "b",
+  "a"
+];
 var konamiCodePosition = 0;
-document.addEventListener('keydown', function (e) {
+document.addEventListener("keydown", function(e) {
   var key = keyc[e.keyCode];
   var requiredKey = konamiCode[konamiCodePosition];
   if (key == requiredKey) {
@@ -31,21 +43,28 @@ document.addEventListener('keydown', function (e) {
 
 function activateCheats() {
   window.scrollTo(0, 0);
-  document.getElementById("me").src = "me.png";
-  for (i = 0; i <= 360; i++) {
+  document.getElementById("me").src =
+    "https://cdn.glitch.com/fa8e25d3-1b37-4b2e-81f3-3972f497af8d%2Fme.png?v=1620396075727";
+  for (var i = 0; i <= 360; i++) {
     setTimeout("rotateit(" + i + ")", i);
   }
-  void(0);
-};
+  void 0;
+}
 
 function rotateit(x) {
   x = parseInt(x);
-  document.body.setAttribute('style', ' -moz-transform: rotate(' + x +
-    'deg); -moz-transform-origin: 50% 50%; -webkit-transform: rotate(' + x +
-    'deg); -webkit-transform-origin: 50% 50%; -o-transform: rotate(' + x +
-    'deg); -o-transform-origin:50% 50%; -ms-transform: rotate(' + x +
-    'deg); -ms-transform-origin: 50% 50%; transform: rotate(' + x + 'deg); transform-origin: 50% 50%;');
-}
-function goback() {
-  window.history.back();
+  document.body.setAttribute(
+    "style",
+    " -moz-transform: rotate(" +
+      x +
+      "deg); -moz-transform-origin: 50% 50%; -webkit-transform: rotate(" +
+      x +
+      "deg); -webkit-transform-origin: 50% 50%; -o-transform: rotate(" +
+      x +
+      "deg); -o-transform-origin:50% 50%; -ms-transform: rotate(" +
+      x +
+      "deg); -ms-transform-origin: 50% 50%; transform: rotate(" +
+      x +
+      "deg); transform-origin: 50% 50%;"
+  );
 }
---
+++ page.js
@@ -0,0 +1,19 @@
+var varSection = window.location.search.substr(1);
+var varArray = varSection.split("&");
+var b = "";
+for (var v = 0; v < varArray.length; v++) {
+  var keyValueArray = varArray[v].split("=");
+
+  if (keyValueArray[0] == "i") {
+    if (keyValueArray[1] == 1) {
+      b = "y";
+    }
+  }
+}
+function goback() {
+  if (b == 1) {
+    window.history.back();
+  } else {
+    location.href = "https://aysh.me";
+  }
+}
